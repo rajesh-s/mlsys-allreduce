d9a5119fdcc0239b107e38982138d93e383160f4
Rajesh Shashi Kumar <35628747+rajesh-s@users.noreply.github.com>
2022-03-22 23:46:51 -0500

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Assumes node0 is at IP 10.10.1.1
+# Assumes worker nodes have hostname node1, node2, ... node15
+# Assumes you can ssh from node0 to all the other nodes.
+
+pytbon3 main.py --master-ip 10.10.1.1 --num-nodes 16 --rank 0
+
+for i in `seq 1 15`
+do
+        RANK=$i
+        echo "Staring rank $RANK"
+        ssh -f node$i "nohup python3 main.py --master-ip 10.10.1.1 --num-nodes 16 --rank $RANK"
+done
