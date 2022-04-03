4f56a2134e5e475154405e162940f496de6d461e
ashwin98 <ashwin.poduval@gmail.com>
2022-04-02 22:21:06 -0500

    Split into world_size parts for bandwidth efficiency

--- ring_allreduce.py
+++ ring_allreduce.py
@@ -14,36 +14,56 @@ def init_process(master_ip, rank, world_size):
                             rank=rank,
                             world_size=world_size)
 
-def main(rank, tensor_size):
+def main(tensor_size):
+    # Get world size and rank
+    world_size = dist.get_world_size()
+    rank = dist.get_rank()
     # Create a random tensor
     t = torch.rand(tensor_size)
-    send_buffers = [torch.zeros(tensor_size) for i in range(0, dist.get_world_size())]
-    recv_buffers = [torch.zeros(tensor_size) for i in range(1, dist.get_world_size())]
-    send_buffers[0] = t
-    s = time.time()
-    for i in range(1, dist.get_world_size()):
+    t = list(t.split(int(tensor_size/world_size)))
+    #print(len(t))
+    #print(tensor_size)
+    #print(world_size)
+    #print(t)
+    # Create send and receive buffers
+    zero_buffer = torch.zeros(tensor_size)
+    recv_buffers = list(zero_buffer.split(int(tensor_size/world_size)))
+    #s = time.time()
+    # Reduce-scatter loop
+    for i in range(1, world_size):
+        if (rank % 2) == 0:
+            # Send a tensor to the previous machine
+            #print((rank + i) % world_size)
+            dist.send(t[(rank + i) % world_size], dst=(rank + world_size - 1) % world_size)
+
+            # Receive a tensor from the next machine
+            dist.recv(recv_buffers[i-1], src=(rank + 1) % world_size)
+        else:
+            # Receive a tensor from the next machine
+            dist.recv(recv_buffers[i-1], src=(rank + 1) % world_size)
+
+            # Send a tensor to the previous machine
+            dist.send(t[(rank + i) % world_size], dst=(rank + world_size - 1) % world_size)
+        # Accumulate value in t. At the end of the for loop, t will hold the reduced value
+        t[(rank + i + 1) % world_size] += recv_buffers[i-1]
+    # All-gather loop
+    for i in range(1, world_size):
         if (rank % 2) == 0:
             # Send a tensor to the next machine
-            dist.send(send_buffers[i-1], dst=(dist.get_rank() + 1) % dist.get_world_size())
+            dist.send(t[(rank + 1 - i + world_size) % world_size], dst=(rank + 1) % world_size)
 
             # Receive a tensor from the previous machine
-            dist.recv(recv_buffers[i-1], src=(dist.get_rank() + dist.get_world_size() - 1) % dist.get_world_size())
-        else:
+            dist.recv(t[(rank - i + world_size) % world_size], src=(rank + world_size - 1) % world_size)
+            else:
             # Receive a tensor from the previous machine
-            dist.recv(recv_buffers[i-1], src=(dist.get_rank() + dist.get_world_size() - 1) % dist.get_world_size())
+            dist.recv(t[(rank - i + world_size) % world_size], src=(rank + world_size - 1) % world_size)
 
             # Send a tensor to the next machine
-            dist.send(send_buffers[i-1], dst=(dist.get_rank() + 1) % dist.get_world_size())
-
-
-        # Update send buffer values
-        send_buffers[i] = recv_buffers[i-1]
-        # Accumulate value in t. At the end of the for loop, t will hold the reduced value
-        t += recv_buffers[i-1]
-    e = time.time()
-    print("Finished allreduce in ", e-s, " seconds")
+            dist.send(t[(rank + 1 - i + world_size) % world_size], dst=(rank + 1) % world_size)
+    #e = time.time()
+    #print("Finished allreduce in ", e-s, " seconds")
     #print(recv_buffers)
-    #print(t)
+    print(torch.cat(t))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -56,4 +76,4 @@ if __name__ == "__main__":
     init_process(master_ip=args.master_ip,
                  rank=args.rank,
                  world_size=args.num_nodes)
-    main(rank=args.rank, tensor_size=args.tensor_size)
+    main(tensor_size=args.tensor_size)
